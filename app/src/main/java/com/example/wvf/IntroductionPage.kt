7f2087f34180550228f529d17f6c5ed827bb0977
JaeHyeon <jaehyeon0825@gmail.com>
2021-12-29 18:15:25 +0700

    스플래쉬/뷰페이저2 (텍스트 작업 X)

--- app/src/main/java/com/example/wvf/IntroductionPage.kt
+++ app/src/main/java/com/example/wvf/IntroductionPage.kt
@@ -2,10 +2,23 @@ package com.example.wvf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 
 class IntroductionPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_introduction_page)
+
+        //뷰페이저 2 코드
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        viewPager.adapter = ViewPagerAdapter(getList())
+        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+    }
+    //뷰페이저2 코드
+    private fun getList(): ArrayList<Int> {
+        return arrayListOf<Int>(R.drawable.intro1, R.drawable.intro2, R.drawable.intro3)
     }
 }
